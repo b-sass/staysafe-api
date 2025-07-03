8374ac58bebfe9fb7a41838827b4fd563157e5cd
Bartosz Sass <sassbartek@gmail.com>
2025-07-03 16:54:59 +0200

    database connection test with sequelize

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,13 @@
-import mariadb from "mariadb"
+import { Sequelize } from "sequelize";
 
-export const pool = mariadb.createPool({
-	host: process.env.DB_ADDRESS,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASS,
-	database: process.env.DB_NAME
-});
+const connection = new Sequelize(
+	process.env.DB_NAME!,
+	process.env.DB_USER!,
+	process.env.DB_PASS,
+	{
+		host: process.env.DB_ADDRESS,
+		dialect: 'mariadb'
+	}
+)
+
+export default connection;
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
-import {pool} from "./db"
-
-import express from "express";
+import connection from './db';
+import express, { Response } from "express";
 
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send("Hello typescript!");
+  
+  testDB(res);
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+async function testDB(res: Response) {
+  try {
+    await connection.authenticate();
+    console.log(process.env.DB_USER)
+    res.send('Connection has been established successfully.');
+    
+  } catch (error) {
+    res.send(`Unable to connect to the database: ${error}`);
+  }
+}
