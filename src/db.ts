b8e145440a16363bff3a56ec14655df975fbec8e
Bartosz Sass <sassbartek@gmail.com>
2025-07-03 16:33:35 +0200

    Typescript conversion.

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,8 @@
 import mariadb from "mariadb"
 
-let pool = mariadb.createPool({
+export const pool = mariadb.createPool({
 	host: process.env.DB_ADDRESS,
-	port: process.env.DB_PORT,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASS,
 	database: process.env.DB_NAME
 });
-
-export default pool
