6ada2ff52baa485bef6fab1fdeba58721c17e644
Bartosz Sass <sassbartek@gmail.com>
2025-07-20 12:14:50 +0100

    User creation endpoint.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import connection from './db';
 import express, { Response } from "express";
 import userRouter from "./routes/users";
-import defaultRouter from "./routes/default"
+import defaultRouter from "./routes/default";
+import bodyParser from 'body-parser';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
+app.use(bodyParser.json());
+app.use(express.json());
 
 app.use("/", defaultRouter);
 app.use("/users", userRouter);
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,7 +1,8 @@
 import { Request, RequestHandler, Response, Router } from "express";
 import { User } from "../models/User";
-
 const router = Router();
+import { Validate, ValidateLogin, ValidateUser } from "../middleware/validate";
+
 
 //Get all users
 router.get("/", async (req: Request, res: Response) => {
@@ -13,9 +14,58 @@ router.get("/", async (req: Request, res: Response) => {
 // Get user by ID
 router.get("/:id", async (req: Request, res: Response) => {
     let userID = req.params.id;
-    let user = await User.findByPk(parseInt(userID));
-    
-    res.status(200).send(user);
+    try {
+        let user = await User.findByPk(parseInt(userID));
+
+        if (!user) { res.status(404).json({ error: "No user found."})}
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json({
+            error: `${err}`
+        })
+    }
 });
+
+router.post(
+    "/",
+    ValidateUser,
+    Validate,
+    async (req: Request, res: Response) => {
+        console.log("POST: user")
+        let userDetails = req.body;
+        try {
+            let user = await User.findOne({
+                where: {
+                    username: userDetails.username
+                }
+            });
+
+            if (user) {
+                res.status(400).json({
+                    error: "User with this username already exists."
+                });
+                return;
+            }
+
+            await User.create({
+                ...userDetails
+            })
+
+            res.status(201).json({
+                message: `User ${userDetails.username} created;`
+            });
+        } catch (err) {
+            res.status(500).json({
+                error: `${err}`
+            });
+        }
+    }
+)
+
+router.post("/login", ValidateLogin,
+    async(req: Request, res: Response) => {
+
+    }
+)
 
 export default router;
